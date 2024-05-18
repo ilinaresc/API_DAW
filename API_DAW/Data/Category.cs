25ca981ea72ddbe241650035458fc4b448076baa
Irving Linares <linares.irving@gmail.com>
2024-05-17 22:48:26 -0500

    Add project files.

---
+++ API_DAW/Data/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_DAW.Data;
+
+public partial class Category
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public bool? IsActive { get; set; }
+}
---
+++ API_DAW/Data/Favorite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_DAW.Data;
+
+public partial class Favorite
+{
+    public int Id { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
